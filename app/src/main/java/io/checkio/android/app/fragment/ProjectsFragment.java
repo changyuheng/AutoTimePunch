d70fbd1765748d0df3945766545a66aba1510500
Chang Yu-heng (張昱珩) <mr.changyuheng@gmail.com>
2014-10-19 20:47:57 +0800

    Let projects be selectable

--- app/src/main/java/io/checkio/android/app/fragment/ProjectsFragment.java
+++ app/src/main/java/io/checkio/android/app/fragment/ProjectsFragment.java
@@ -1,10 +1,12 @@
 package io.checkio.android.app.fragment;
 
+import android.app.Activity;
 import android.app.ListFragment;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
+import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,13 +19,23 @@ import android.widget.TextView;
 import io.checkio.android.app.R;
 import io.checkio.android.app.database.PunchDatabaseHelper;
 
-public class ProjectsFragment extends ListFragment {
+public class ProjectsFragment extends ListFragment implements ListView.MultiChoiceModeListener {
+
+    private Activity mActivity;
+    private MenuItem mEditMenuItem;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
      */
     public ProjectsFragment() {
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        mActivity = activity;
     }
 
     @Override
@@ -48,15 +60,15 @@ public class ProjectsFragment extends ListFragment {
     }
 
     private void updateAdapter() {
-        SQLiteDatabase db = PunchDatabaseHelper.getInstance(getActivity()).getReadableDatabase();
+        SQLiteDatabase db = PunchDatabaseHelper.getInstance(mActivity).getReadableDatabase();
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
         qb.setTables(PunchDatabaseHelper.Tables.PROJECT);
 
         Cursor c = qb.query(db, PunchDatabaseHelper.PROJECT_PROJECTION, null,
                 null, null, null, null);
 
-        ListAdapter adapter = new SimpleCursorAdapter(getActivity(),
-                android.R.layout.simple_list_item_1, c,
+        ListAdapter adapter = new SimpleCursorAdapter(mActivity,
+                android.R.layout.simple_list_item_activated_1, c,
                 new String[] {PunchDatabaseHelper.ProjectColumns.DISPLAY_NAME},
                 new int[] {android.R.id.text1}, 0);
 
@@ -65,9 +77,9 @@ public class ProjectsFragment extends ListFragment {
 
     private void updateUi() {
         setHasOptionsMenu(true);
-        getActivity().getActionBar().setDisplayHomeAsUpEnabled(false);
-        getActivity().getActionBar().setHomeButtonEnabled(false);
-        getActivity().getActionBar().setTitle(R.string.app_name);
+        mActivity.getActionBar().setDisplayHomeAsUpEnabled(false);
+        mActivity.getActionBar().setHomeButtonEnabled(false);
+        mActivity.getActionBar().setTitle(R.string.app_name);
     }
 
     @Override
@@ -86,14 +98,17 @@ public class ProjectsFragment extends ListFragment {
     }
 
     private void initLayout(View rootView) {
-        setListViewPadding();
-    }
-
-    private void setListViewPadding() {
         ListView listView = getListView();
 
+        setListViewPadding(listView);
+
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(this);
+    }
+
+    private void setListViewPadding(ListView listView) {
         // 16dp is defined in http://developer.android.com/design/style/metrics-grids.html#examples
-        int pixels = (int) (16 * getActivity().getResources().getDisplayMetrics().density + 0.5f);
+        int pixels = (int) (16 * mActivity.getResources().getDisplayMetrics().density + 0.5f);
 
         listView.setPadding(pixels, 0, pixels, 0);
         listView.setClipToPadding(false);
@@ -110,5 +125,43 @@ public class ProjectsFragment extends ListFragment {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+        final int checkedCount = getListView().getCheckedItemCount();
+        switch (checkedCount) {
+            case 0:
+                mode.setSubtitle(null);
+                break;
+            case 1:
+                mEditMenuItem.setVisible(true);
+                break;
+            default:
+                mEditMenuItem.setVisible(false);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        MenuInflater inflater = mActivity.getMenuInflater();
+        inflater.inflate(R.menu.select_projects, menu);
+        mEditMenuItem = menu.findItem(R.id.action_delete);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+        return true;
+    }
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
     }
 }
